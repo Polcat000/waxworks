56dd5696ba0767c665f05f72b43ccf613f9d3820
Polcat000 <andrewpolcaster@gmail.com>
2018-03-24 12:27:19 -0400

    updates to all files, added gmaps.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+		var mapObj = new GMaps({
+		  div: "#map",
+		  lat: 38.237162,
+		  lng: -85.727320,
+		  zoom: 13
+		});
 
+		var m = mapObj.addMarker({
+  			lat: 38.237162,
+		  	lng: -85.727320,
+  			title: 'WaxWorks'
+  			
+		});
+		
 });
